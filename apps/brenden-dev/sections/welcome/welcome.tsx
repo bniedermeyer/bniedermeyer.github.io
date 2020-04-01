950867598baf775f91be9edff336b1460f6cbdce
Brenden <brenden.niedermeyer@gmail.com>
2020-04-01 11:53:23 -0700

    add image in welcome

--- apps/brenden-dev/sections/welcome/welcome.tsx
+++ apps/brenden-dev/sections/welcome/welcome.tsx
@@ -7,18 +7,27 @@ import { ConfiguredParticles } from '@personal-site/components';
 export const Welcome = () => {
   return (
     <div className="welcome">
-      <div className="welcome-text-container">
-        <div className="welcome-text">
-          <h1>Hi! I'm Brenden</h1>
-          <h2>I'm a web developer in Seattle</h2>
-          <h3>
-            I specialize in building powerful apps with modern web technologies.
-            Learn more about me at my <a href="https://brenden.codes">blog</a>,
-            or <a href="https://brenden.fyi">resume</a>.<br />
-            You can see some of my work <a href="#info">here</a>.
-          </h3>
+      <div className="welcome-content-container">
+        <img
+          className="talk-photo"
+          src="/speaking.jpg"
+          alt="Speaking at Angular Seattle"
+        />
+        <div className="welcome-text-container">
+          <div className="welcome-text">
+            <h1>Hi! I'm Brenden</h1>
+            <h2>I'm a web developer in Seattle</h2>
+            <h3>
+              I specialize in building powerful apps with modern web
+              technologies. Learn more about me at my{' '}
+              <a href="https://brenden.codes">blog</a>, or{' '}
+              <a href="https://brenden.fyi">resume</a>. You can see some of my
+              work <a href="#info">here</a>.
+            </h3>
+          </div>
         </div>
       </div>
+
       <ConfiguredParticles height={'100vh'} width={'100%'} />
     </div>
   );
