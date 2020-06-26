3e023fd96dd70ca3f3f92666555c33951a9eda2d
Brenden Niedermeyer <contact@brenden.dev>
2020-06-26 11:52:38 -0700

    feat: update head meta tags

--- apps/brenden-dev/pages/index.tsx
+++ apps/brenden-dev/pages/index.tsx
@@ -19,10 +19,16 @@ export const Index = () => {
   return (
     <div className="app">
       <Head>
+        <title>Brenden Niedermeyer</title>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0"
+          content="width=device-width, initial-scale=1.0, viewport-fit=cover"
           key="viewport"
+        />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta
+          name="description"
+          content="Homepage for Brenden Niedermeyer - A software developer in Seattle, WA"
         />
       </Head>
       <Navbar />
