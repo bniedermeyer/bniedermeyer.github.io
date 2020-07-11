8c5c58ce053c4908e4e72212bb2ac1fcc0bbccaa
Brenden Niedermeyer <contact@brenden.dev>
2020-07-11 14:33:25 -0700

    add cascadia discount link

---
+++ apps/brenden-dev/pages/cascadia.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+export const Cascadia = () => {
+  useEffect(() => {
+    window.location =
+      'https://ti.to/event-loop/cascadiajs-2020/discount/brenden';
+  });
+  return <div></div>;
+};
+
+export default Cascadia;
