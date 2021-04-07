220706741124a63aed4ed42ba4a011c4e4815762
Brenden Niedermeyer <contact@brenden.dev>
2021-04-07 20:40:17 +0200

    feat: refactor components to be within project vs library

--- libs/components/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './lib/dynamic-storyblok/dynamic-storyblok';
-export * from './lib/navbar/navbar';
-export * from './lib/logo/logo';
-export * from './lib/configured-particles/configured-particles';
-export * from './lib/card/card';
