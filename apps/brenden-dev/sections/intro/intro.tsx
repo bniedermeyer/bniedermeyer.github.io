ca27e89c940d8b170d0b76131ea276f1974a2352
Brenden Niedermeyer <contact@brenden.dev>
2023-01-02 17:34:32 +0100

    fix: correct typo and adjust word spacing in intro

--- apps/brenden-dev/sections/intro/intro.tsx
+++ apps/brenden-dev/sections/intro/intro.tsx
@@ -29,28 +29,29 @@ export function Intro() {
             priority
           />
         </div>
-        <div className="px-4">
+        <div className="px-5 md:px-2 tracking-tighter">
           <h1 className="text-purple-800 dark:text-primary-light text-center md:text-left text-7xl font-extrabold">
             Hi! I&apos;m Brenden
           </h1>
           <h2 className="text-purple-800 dark:text-primary-light text-center md:text-left text-4xl">
             I&apos;m a web developer
           </h2>
-          <p className="text-primary-dark dark:text-cyan-400 mt-2 md:mt-0 text-2xl text-justify md:text-left">
-            I specialise in building powerfull apps with modern web{' '}
-            <br className="md:hidden" />
-            technologies. <br className="hidden md:block" /> Learn more about me
-            at my{' '}
+          <p className="text-primary-dark dark:text-cyan-400 mt-4 text-2xl lg:text-justify md:w-4/5 ">
+            I specialize in building powerfull apps with modern web
+            technologies.
+          </p>
+          <p className="text-primary-dark dark:text-cyan-400 mt-5 md:mt-10 text-2xl md:text-justify">
+            Learn more about me at my
             <a
               href="https://brenden.codes"
-              className="text-purple-800 dark:text-secondary-light underline decoration-2 decoration-yellow-500"
+              className="text-purple-800 dark:text-secondary-light underline decoration-2 decoration-yellow-500 ml-1"
             >
               blog
             </a>
-            , or{' '}
+            , or
             <a
               href="https://brenden.fyi"
-              className="text-purple-800 dark:text-secondary-light underline decoration-2 decoration-emerald-500"
+              className="text-purple-800 dark:text-secondary-light underline decoration-2 decoration-emerald-500 ml-1"
             >
               cv
             </a>
