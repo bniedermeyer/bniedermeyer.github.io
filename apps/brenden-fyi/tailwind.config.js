c40cbea9da35a83470fb20d39e354622ba2028f8
Brenden Niedermeyer <contact@brenden.dev>
2023-04-23 15:07:57 +0200

    feat: add footer

--- apps/brenden-fyi/tailwind.config.js
+++ apps/brenden-fyi/tailwind.config.js
@@ -8,7 +8,28 @@ module.exports = {
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        primary: {
+          dark: '#2d334a',
+          DEFAULT: '#2d334a',
+          light: '#fffffe',
+        },
+        secondary: {
+          dark: '#d4d8f0',
+          default: '#d4d8f0',
+          light: '#e3f6f5',
+        },
+        tertiary: {
+          dark: '#b8c1ec',
+          DEFAULT: '#b8c1ec',
+          light: '#bae8e8',
+        },
+      },
+    },
   },
   plugins: [],
 };
